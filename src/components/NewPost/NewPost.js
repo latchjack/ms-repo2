f10e2cd411dc30472d0272b2846aac362bbedf05
Latch Jackn <latch.jack@gmail.com>
2020-11-19 15:12:57 +0000

    added post functionality

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -7,6 +8,30 @@ class NewPost extends Component {
     title: '',
     content: '',
     author: 'Max'
+  }
+
+  postDataHandler = () => {
+    /*
+    |=====================================
+    | Axios will send our data object in a 
+    | json format to store on the server. 
+    |=====================================
+    */
+    const data = {
+      title: this.state.title,
+      body: this.state.body,
+      author: this.state.author
+    };
+    /*
+    |=====================================
+    | Here we pass the data object to the POST url.
+    | This runs asynchronously.
+    |=====================================
+    */
+    axios.post('https://jsonplaceholder.typicode.com/posts', data)
+      .then(response => {
+        console.log(response);
+      });
   }
 
   render () {
@@ -22,7 +47,7 @@ class NewPost extends Component {
           <option value="Max">Max</option>
           <option value="Manu">Manu</option>
         </select>
-        <button>Add Post</button>
+        <button onClick={this.postDataHandler}>Add Post</button>
       </div>
     );
   }
