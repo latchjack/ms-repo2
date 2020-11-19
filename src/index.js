3fd6219a348993b8c54bd5c4638af6606a7ccc8e
Latch Jackn <latch.jack@gmail.com>
2020-11-19 20:16:57 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+/*
+|==============================================================
+| This is the most global file in our app, as this is where it all is exported from. 
+| So we add this here so that it can ,... any requests made with axios in our app.
+|==============================================================
+*/
+axios.interceptors
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
