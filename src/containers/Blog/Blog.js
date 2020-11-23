b01dfb6e952c84ff25265e30043de9f7cf4dcba1
Latch Jackn <latch.jack@gmail.com>
2020-11-23 13:25:21 +0000

    added Posts component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,101 +1,14 @@
 import React, { Component } from 'react';
 import './Blog.css';
-
 // import axios from 'axios';
-import axios from '../../axios';
-/*
-|==============================================================
-| Here we import axios from the axios.js file. 
-| Remember that afer the word import these are just variable names
-| We could called this axiosInstance if we'd like
-| (import axiosInstance from '../../axios';).
-|==============================================================
-*/
-
+// import axios from '../../axios';
+import Posts from '../Blog/Posts/Posts';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  }
 
-  componentDidMount() {
-    /*
-    |==============================================================
-    | To control the amount of posts we render on the screen at once,
-    | we used the slice method to control the desired amount (0 > 4).
-    | This way the fetch method will still fetch all of the posts,
-    | but it will only store four of them in the posts array, which
-    | is then mapped over in the updatedPosts arrow function below it.
-    |
-    | Next, instead of setting state to response.data, we set it to 
-    | updatedPosts to hardcode the Author field to the data, as the 
-    | backend doesn't provide this field. First we spread the mapped
-    | post and then we added the author field with the name 'Max'.
-    |==============================================================
-    */
-    axios.get('/posts')
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => {
-          return {
-            ...post,
-            author: 'Max'
-          }
-        });
-        /*
-        |==============================================================
-        | Here I've set state to the updatedPosts constant as mentioned
-        | above. Usually I would set this to 'response.data' which would
-        | store all of the fetched data into state. This way using the
-        | functions above I'm able to manipulate the data first before
-        | storing it into state.
-        |==============================================================
-        */
-        this.setState({ posts: updatedPosts })
-        // console.log(response);
-        // console.log(response.data);
-      })
-    .catch(err => {
-      this.setState({ error: true });
-    })
-  }
-
-  postSelectedHandler = (id) => {
-    /*
-    |==============================================================
-    | This takes the post.id argument and sets it to state inside
-    | selectedPostId. This will allow us to render the post in the
-    | FullPost component later on.
-    |==============================================================
-    */
-    this.setState({ selectedPostId: id });
-  }
+  
 
   render() {
-    let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        /*
-        |==============================================================
-        | Here we pass props down to the Post component.
-        | key - gives each post its own unique key (required by react).
-        | title - allows the variable title in the Post component
-        | author - hardcoded above and passes it in component to render.
-        | clicked - es6 function will take the post id as an argument 
-        | and pass it to the postSelectHandler to run function on that 
-        | particular post.
-        |==============================================================
-        */
-        return <Post 
-          key={post.id} 
-          title={post.title} 
-          author={post.author} 
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
-      });
-    }
     return (
       <div className="Blog">
         <header>
@@ -106,9 +19,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">
-          {posts}
-        </section>
+        <Posts />
       </div>
     );
   }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import Post from '../../../components/Post/Post';
+import './Posts.module.css';
+import axios from '../../../axios';
+/*
+|==============================================================
+| Here we import axios from the axios.js file. 
+| Remember that afer the word import these are just variable names
+| We could called this axiosInstance if we'd like
+| (import axiosInstance from '../../axios';).
+|==============================================================
+*/
+
+class Posts extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    /*
+    |==============================================================
+    | To control the amount of posts we render on the screen at once,
+    | we used the slice method to control the desired amount (0 > 4).
+    | This way the fetch method will still fetch all of the posts,
+    | but it will only store four of them in the posts array, which
+    | is then mapped over in the updatedPosts arrow function below it.
+    |
+    | Next, instead of setting state to response.data, we set it to 
+    | updatedPosts to hardcode the Author field to the data, as the 
+    | backend doesn't provide this field. First we spread the mapped
+    | post and then we added the author field with the name 'Max'.
+    |==============================================================
+    */
+    axios.get('/posts')
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Max'
+          }
+        });
+        /*
+        |==============================================================
+        | Here I've set state to the updatedPosts constant as mentioned
+        | above. Usually I would set this to 'response.data' which would
+        | store all of the fetched data into state. This way using the
+        | functions above I'm able to manipulate the data first before
+        | storing it into state.
+        |==============================================================
+        */
+        this.setState({ posts: updatedPosts })
+        // console.log(response);
+        // console.log(response.data);
+      })
+    .catch(err => {
+      console.log(err);
+      // this.setState({ error: true });
+    })
+  }
+
+  postSelectedHandler = (id) => {
+    /*
+    |==============================================================
+    | This takes the post.id argument and sets it to state inside
+    | selectedPostId. This will allow us to render the post in the
+    | FullPost component later on.
+    |==============================================================
+    */
+    this.setState({ selectedPostId: id });
+  }
+
+  render() {
+    let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        /*
+        |==============================================================
+        | Here we pass props down to the Post component.
+        | key - gives each post its own unique key (required by react).
+        | title - allows the variable title in the Post component
+        | author - hardcoded above and passes it in component to render.
+        | clicked - es6 function will take the post id as an argument 
+        | and pass it to the postSelectHandler to run function on that 
+        | particular post.
+        |==============================================================
+        */
+        return <Post 
+          key={post.id} 
+          title={post.title} 
+          author={post.author} 
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      });
+    }
+
+    return(
+      <section className="Posts">
+        {posts}
+      </section>
+    )
+  }
+}
+
+export default Posts;
