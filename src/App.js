ec7b311eadca84e79cef9a62c9ac97cf511f30b0
Latch Jackn <latch.jack@gmail.com>
2020-11-24 16:18:36 +0000

    added notes to App.js

--- src/App.js
+++ src/App.js
@@ -15,4 +15,27 @@ class App extends Component {
   }
 }
 
+/*
+|=============================================
+| If the domain that you're hosting your app is
+| a subdomain for example, www.example.com/my-app
+| you have to set the basename attribute in the 
+| BrowserRouter component. Otherwise when trying
+| to navigate around the app your routes will not
+| be appended to the address correctly.
+|
+| You will be directed to...
+| www.example.com/posts
+|
+| Instead of...
+| www.example.com/my-app/posts
+|
+| <BrowserRouter basename="/my-app">
+|   <div className="App">
+|     <Blog />
+|   </div>
+| </BrowserRouter>
+|=============================================
+*/
+
 export default App;
